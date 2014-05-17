4d87e1574eb949d121eb257cdb197b7a0b0135bb
Bojan Percevic <percevic@icloud.com>
2014-05-17 15:55:20 -0400

    check for empty object

--- controllers/util.js
+++ controllers/util.js
@@ -234,6 +234,9 @@ function _valueForKeyInDictionary(key, dictionary) {
 			}
 			return data;
 		} else {
+			if (isEmpty(dictionary[key])) {
+				return {};
+			}
 			var value = {};
 			for (var subkey in dictionary[key]) {
 				value[subkey] = _valueForKeyInDictionary(subkey, dictionary[key]);
@@ -299,4 +302,24 @@ exports.res = function(message, data) {
 		retVal[data] = data;
 	}
 	return retVal;
+}
+
+function isEmpty(obj) {
+
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
 }
