8a66f9d7b613d4996b6ef65f24a0a9da4cfe41be
Bojan Percevic <percevic@icloud.com>
2014-05-17 15:02:18 -0400

    removed unused data field from response

--- controllers/util.js
+++ controllers/util.js
@@ -287,8 +287,11 @@ exports.error = function(error, res) {
 }
 
 exports.res = function(message, data) {
-	return {
-		message : message,
-		data : data || {}
+	var retVal = {
+		message : message
 	};
+	if (data) {
+		retVal[data] = data;
+	}
+	return retVal;
 }
