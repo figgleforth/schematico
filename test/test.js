4ffaf3bb442c3354461a560237cb197c790e01ae
Bojan Percevic <percevic@icloud.com>
2014-05-18 18:10:50 -0400

    forgot to change post to put for update

--- test/test.js
+++ test/test.js
@@ -77,7 +77,7 @@ describe("API", function() {
 	});
 
 	it("Update a route without token should return 400", function(done) {
-		supertest(app).post("/"+_username+"/"+_route).send({
+		supertest(app).put("/"+_username+"/"+_route).send({
 			"name" : "Name",
 			"phone" : "Phone"
 		}).expect(200).end(function(error, res) {
@@ -86,7 +86,7 @@ describe("API", function() {
 	});
 
 	it("Update a route with token should return 200", function(done) {
-		supertest(app).post("/"+_username+"/"+_route+"?token="+_newUserToken).send({
+		supertest(app).put("/"+_username+"/"+_route+"?token="+_newUserToken).send({
 			"name" : "Name",
 			"phone" : "Phone"
 		}).expect(200).end(function(error, res) {
