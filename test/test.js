cb8a5acfab210e2a55275277ce5b31a8ed9f6ef8
Bojan Percevic <percevic@icloud.com>
2014-05-18 19:18:19 -0400

    what, the tests finally work?

--- test/test.js
+++ test/test.js
@@ -97,7 +97,7 @@ describe("API", function() {
 		.set("Content-Type", "application/json")
 		.send(newSchema)
 		.end(function(error, res) {
-			res.should.have.status(400);
+			res.should.have.status(401);
 			done();
 		});
 	});
